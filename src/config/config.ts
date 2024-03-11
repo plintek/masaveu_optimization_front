bb2db5bb0d81e6114fe35beb5e148f22afd4d242
David Espejo Antiñolo <davidespejoantinolo@gmail.com>
2024-03-11 20:30:10 +0100

    Add comment

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ const env = import.meta.env;
 const config: Config = {
     name: "masaveu_optimization_front",
     env: env.VITE_ENV || "production",
-    endpoint_url: env.VITE_ENDPOINT_URL || "http://localhost:8000/api/v1/",
+    endpoint_url: env.VITE_ENDPOINT_URL || "http://localhost:8000/api/v1/", //"http://devctv.dotgiscorp.com:8000/api/v1/"
     routes,
     recaptcha: {
         siteKey: "6Lcof8IfAAAAAEy9uFD_de4wR--y_GPw68tqgme3",
